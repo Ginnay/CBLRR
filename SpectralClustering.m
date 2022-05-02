6be43b61e1ff509c6e991837f03f8142ad2d6704
Ginnay <50011577+Ginnay@users.noreply.github.com>
2022-05-02 10:57:14 +0800

    Update SpectralClustering.m

--- SpectralClustering.m
+++ SpectralClustering.m
@@ -1,5 +1,5 @@
 %--------------------------------------------------------------------------
-%This function takes an adjacency matrix of a graph and computes the 
+% This function takes an adjacency matrix of a graph and computes the 
 % clustering of the nodes using the spectral clustering algorithm of 
 % Ng, Jordan and Weiss.
 % CMat: NxN adjacency matrix
@@ -15,8 +15,8 @@ function [groups, kerNS] = SpectralClustering(CKSym,n)
 
 warning off;
 N = size(CKSym,1);
-MAXiter = 1000; # Maximum number of iterations for KMeans 
-REPlic = 100; # Number of replications for KMeans
+MAXiter = 1000; % Maximum number of iterations for KMeans 
+REPlic = 100; % Number of replications for KMeans
 
 % Normalized spectral clustering according to Ng & Jordan & Weiss
 % using Normalized Symmetric Laplacian L = I - D^{-1/2} W D^{-1/2}
@@ -25,11 +25,10 @@ DN = diag(1./sqrt(sum(CKSym)+eps) );
 LapN = speye(N) - DN * CKSym * DN;
 [~,~,vN] = svd(LapN);
 kerN = vN(:,N-n+1:N);
-
+%kerN = vN(:,N-12:N);
 normN = sum(kerN .^2, 2) .^.5;
 kerNS = bsxfun(@rdivide, kerN, normN + eps);
 
-%-------------
 %Y = pdist(kerNS);
 %Z = linkage(Y);
 %groups = cluster(Z,'maxclust',n);
