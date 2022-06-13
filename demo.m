f4177a974d8ad50116ffc911ac9c893b96124e9d
Ginnay <50011577+Ginnay@users.noreply.github.com>
2022-06-13 12:52:09 +0800

    Update demo.m

--- demo.m
+++ demo.m
@@ -1,14 +1,20 @@
 clc
 clear
 
-load('in_X.mat') %Loading data, each column denotes a gene and each row denotes a cell.
-load('true_labs.mat') %Loading labels.
+%% input:
+% The expression matrix of scRNA-seq data used in the CBLRR method.Taking the kolod data matrix as an example, the rows are the genes (10685) and the columns are the samples (704).
+% The labels of scRNA-seq data used in the CBLRR method. Taking the Kolod labels as an example, it contains 5 types (1, 2, ..., 5).
+load('Examples\in_X.mat') %Loading data, each column denotes a gene and each row denotes a cell.
+load('Examples\true_labs.mat') %Loading labels.
 
-alpha=1;
-mu =110;
-a = 10;
-beta = 1;
+%% set tuning parameters:
+alpha=1; % set as 1 by default.
+mu =110; % set as 110 by default.
+a = 10; % set as 10 by default.
+beta = 1; %set as 1 by default.
 n_space = length(unique(true_labs));% The cluster is predefine:
-
 %[n_space,eigenvalues] = cal_eigenvalues(Z);% The number of cluster is not given:
+
+%% perform CBLRR:
 [NMI,ARI,grps,similarity,Z] = CBLRR(in_X,true_labs,n_space,alpha,beta,mu,a);
+
